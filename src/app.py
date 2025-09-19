cee1b40fb8cb38ae20d8e77eb8863954231d0909
tronxi <sergio.gsanchez97@gmail.com>
2025-09-19 17:55:44 +0200

    refactor

--- src/app.py
+++ src/app.py
@@ -1,4 +1,37 @@
+import subprocess
+
+from PyQt6.QtCore import QFileInfo, Qt
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QFileIconProvider
+
+
 class App:
     def __init__(self, name, path):
         self.name = name
         self.path = path
+        self._provider = QFileIconProvider()
+
+    def draw(self):
+        icon = QWidget()
+
+        icon_image = self._provider.icon(QFileInfo(self.path))
+        icon_pixmap = icon_image.pixmap(80, 80)
+
+        icon_layout = QVBoxLayout(icon)
+        icon_layout.setContentsMargins(10, 10, 10, 10)
+        icon_layout.setSpacing(6)
+
+        icon_lbl = QLabel()
+        icon_lbl.setPixmap(icon_pixmap)
+
+        name_lbl = QLabel(self.name[:-4])
+        name_lbl.setWordWrap(True)
+        name_lbl.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+
+        icon_layout.addWidget(icon_lbl)
+        icon_layout.addWidget(name_lbl)
+        icon.setCursor(Qt.CursorShape.PointingHandCursor)
+        icon.mouseReleaseEvent = lambda _: self._run_app()
+        return icon
+
+    def _run_app(self):
+        subprocess.run(["open", self.path])
--- src/main.py
+++ src/main.py
@@ -1,17 +1,8 @@
 import sys
-
-from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QScrollArea, QVBoxLayout, QFrame, \
-    QFileIconProvider, QSizePolicy
-from PyQt6.QtCore import Qt, QFileInfo
-import subprocess
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QScrollArea
+from PyQt6.QtCore import Qt
 from src.app_searcher import AppSearcher
 from AppKit import NSWorkspace, NSScreen
-
-
-def _run_app(app):
-    subprocess.run(["open", app.path])
-
 
 def get_main_wallpaper_path():
     screen = NSScreen.mainScreen()
@@ -25,7 +16,6 @@ class Main(QScrollArea):
 
         self._searcher = AppSearcher()
         self._searcher.search()
-        self._provider = QFileIconProvider()
 
         self.setWindowTitle("Launchpad")
         self._build_ui()
@@ -58,35 +48,11 @@ class Main(QScrollArea):
         cols = 6
         for i, app in enumerate(self._searcher.get_apps()):
             r, c = divmod(i, cols)
-            icon = self._build_icon(app)
+            icon = app.draw()
 
             frm.addWidget(icon, r, c, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
 
         self.setWidget(inner)
-
-    def _build_icon(self, app):
-        icon = QWidget()
-
-        icon_image = self._provider.icon(QFileInfo(app.path))
-        icon_pixmap = icon_image.pixmap(80, 80)
-
-        icon_layout = QVBoxLayout(icon)
-        icon_layout.setContentsMargins(10, 10, 10, 10)
-        icon_layout.setSpacing(6)
-
-        icon_lbl = QLabel()
-        icon_lbl.setPixmap(icon_pixmap)
-        # icon_lbl.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
-
-        name_lbl = QLabel(app.name[:-4])
-        name_lbl.setWordWrap(True)
-        name_lbl.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
-
-        icon_layout.addWidget(icon_lbl)
-        icon_layout.addWidget(name_lbl)
-        icon.setCursor(Qt.CursorShape.PointingHandCursor)
-        icon.mouseReleaseEvent = lambda _: _run_app(app)
-        return icon
 
 
 if __name__ == "__main__":
