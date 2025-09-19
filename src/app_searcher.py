523724c205b38434e1604aac1cebb732e3616085
tronxi <sergio.gsanchez97@gmail.com>
2025-09-19 17:51:03 +0200

    use os.walk

--- src/app_searcher.py
+++ src/app_searcher.py
@@ -14,12 +14,16 @@ class AppSearcher:
         ]
 
     def search(self):
+        self._apps.clear()
         for root in self._roots:
-            apps = os.listdir(root)
-            for app in apps:
-                if app.endswith(".app"):
-                    path = os.path.realpath(os.path.join(root, app))
-                    self._apps.append(App(app, path))
+            if not os.path.isdir(root):
+                continue
+            for dirpath, dirnames, filenames in os.walk(root):
+                for d in dirnames:
+                    if d.endswith(".app"):
+                        full_path = os.path.join(dirpath, d)
+                        self._apps.append(App(d, os.path.realpath(full_path)))
+                dirnames[:] = [dn for dn in dirnames if not dn.endswith(".app")]
 
     def get_apps(self):
         return self._apps
